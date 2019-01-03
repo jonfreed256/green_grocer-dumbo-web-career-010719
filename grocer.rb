fea40d2b83f22cdbdd1aa180d8ae34841e6b3377
github username <github email address>
2019-01-03 09:11:47 -0500

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,21 @@
+require.pry
 def consolidate_cart(cart)
-  # code here
+	binding.pry
+	# consolidated_cart = {}
+	# cart.each do |item_hashes|
+	# 	binding.pry
+ # 		# [{"AVOCADO" => {:price => 3.0, :clearance => true }}]
+	#   	item_hashes.each do |item, info_hash|
+	#   		# {"AVOCADO" => {:price => 3.0, :clearance => true }}
+	#   		if consolidated_cart[item] == nil
+	#   		 	consolidated_cart[item] = {}
+	#   		end
+	#   		item.each_with_object(Hash.new(0)) { |items, counts| consolidated_cart[items][info_hash][:counts][counts] += 1 }
+	#   		# consolidated_cart[item][:count] = 
+	#   	end
+	# end
+ #  consolidated_cart
+ #  # returns {"AVOCADO" => {:price => 3.0, :clearance => true, :count => 2}}
 end
 
 def apply_coupons(cart, coupons)
@@ -10,6 +26,6 @@ def apply_clearance(cart)
   # code here
 end
 
-def checkout(cart, coupons)
+def checkout(cart, coupons)		# calculate total cost of a cart of items and apply discounts and coupons as necessary
   # code here
 end
